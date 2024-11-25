cc9fa6cf013a62eaf32f9287e728df4e75c68e32
Shahanur Rashid Shafin <shafi.ncs3092@gmail.com>
2024-11-25 09:49:41 +0600

    Add files via upload

---
+++ A_Minimize.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main()
+{
+optimize();
+
+int tc;
+cin >> tc;
+while (tc--)
+{
+    int a, b;
+    cin >> a >> b;
+
+    int c = b-a;
+    cout << c << endl;
+}
+
+    return 0;
+}
