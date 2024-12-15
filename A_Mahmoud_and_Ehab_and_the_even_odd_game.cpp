47e2ea99a35405d3a9fdeadbee1de463cb5485f1
Shahanur Rashid Shafin <shafi.ncs3092@gmail.com>
2024-12-15 22:42:13 +0600

    Add files via upload

---
+++ A_Mahmoud_and_Ehab_and_the_even_odd_game.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main()
+{
+optimize();
+
+int num;
+cin >> num;
+if (num % 2 == 0)
+{
+    cout << "Mahmoud" << endl;
+}
+else
+{
+    cout << "Ehab" << endl;
+}
+
+
+    return 0;
+}
